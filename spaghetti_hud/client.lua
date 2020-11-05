1b0ab01bffdfedc73b40966245b674262e613fb2
IAmRSG <34017709+IAmRSG@users.noreply.github.com>
2020-11-04 21:47:31 -0500

    Fixed issue with client console errors
    If the character wasn't loaded, then the metabolism was returning a nil value flooding the client console during character selection. Added a check to see if player has selected a character.

--- spaghetti_hud/client.lua
+++ spaghetti_hud/client.lua
@@ -1,5 +1,6 @@
 money = 0
 mystress = nil
+isloaded = false
 
 RegisterNetEvent("gui:getItems")
 AddEventHandler("gui:getItems", function(THEITEMS)
@@ -7,6 +8,12 @@ AddEventHandler("gui:getItems", function(THEITEMS)
        items = THEITEMS,
     })
 end)
+
+RegisterNetEvent("vorp:SelectedCharacter")
+AddEventHandler("vorp:SelectedCharacter", function(charid)
+    isloaded = true
+end)
+
 --[[
 RegisterNetEvent("gui:getStress")
 AddEventHandler("gui:getStress", function(stress)
@@ -35,35 +42,48 @@ end)]]
 
 Citizen.CreateThread(function()
     while true do
-        local _source = source 
-	--	TriggerServerEvent("hud:checkmoney")
-        Citizen.InvokeNative(0x50C803A4CD5932C5 , true)
-        local myhunger
-        local mythirst
-        local player = PlayerPedId()
-        local coords = GetEntityCoords(player)
-        Citizen.InvokeNative(0xB98B78C3768AF6E0,true)
-        local temp = GetTemperatureAtCoords(coords)
-        local _src = source
+        Citizen.Wait(10)
+        if isloaded then
+            local _source = source 
+        --	TriggerServerEvent("hud:checkmoney")
+            Citizen.InvokeNative(0x50C803A4CD5932C5 , true)
+            local myhunger
+            local mythirst
+            local player = PlayerPedId()
+            local coords = GetEntityCoords(player)
+            Citizen.InvokeNative(0xB98B78C3768AF6E0,true)
+            local temp = GetTemperatureAtCoords(coords)
+            local _src = source
 
-        TriggerEvent("vorpmetabolism:getValue","Thirst",function(value)
-            mythirst = value / 10
-        end)
-        TriggerEvent("vorpmetabolism:getValue","Hunger",function(value)
-            myhunger = value / 10
-        end)
+            TriggerEvent("vorpmetabolism:getValue","Thirst",function(value)
+                if value == nil then
+                    mythirst = 0
+                else
+                    mythirst = value / 10
+                end
+            end)
+            
+            TriggerEvent("vorpmetabolism:getValue","Hunger",function(value)  
+                if value == nil then
+                    myhunger = 0
+                else
+                    myhunger = value / 10
+                end
+            end)
 
-        SendNUIMessage({
-            action = "updateStatusHud",
-                show = not IsRadarHidden(),
-                hunger = myhunger,
-                thirst = mythirst,
-               -- stress = mystress,
-               -- cash = money,
-                temp = math.floor(temp * 1.8 + 32.0).."°F", 
-				--temp= math.floor(temp).."°C", -- You can switch to C by commenting the line above, and uncommenting this line.
-		})
-        Citizen.Wait(1000)
+
+            SendNUIMessage({
+                action = "updateStatusHud",
+                    show = not IsRadarHidden(),
+                    hunger = myhunger,
+                    thirst = mythirst,
+                -- stress = mystress,
+                -- cash = money,
+                    temp = math.floor(temp * 1.8 + 32.0).."°F", 
+                    --temp= math.floor(temp).."°C", -- You can switch to C by commenting the line above, and uncommenting this line.
+            })
+            Citizen.Wait(1000)
+        end
     end
 end)
 --[[
